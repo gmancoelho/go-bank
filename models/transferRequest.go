fb98ee04c02ae7ba91ac2942f602f9f9078db877
bi002155 <guilherme.coelho@inter.co>
2025-04-03 17:19:36 -0300

    feat: fix lint warnings

--- models/transferRequest.go
+++ models/transferRequest.go
@@ -2,6 +2,6 @@ package models
 
 type TransferRequest struct {
 	ToAccountID   int     `json:"toAccountID"`
-	FromAccountID int     `json: "fromAccountID"`
+	FromAccountID int     `json:"fromAccountID"`
 	Amount        float64 `json:"amount"`
 }
